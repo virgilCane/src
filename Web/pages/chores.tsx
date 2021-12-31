966b640d1f2a429c5acf4b6c519937f12533a6ae
CallumLandsberg <33540459+virgilCane@users.noreply.github.com>
2021-12-31 14:34:55 +0200

    chores page

--- Web/pages/chores.tsx
+++ Web/pages/chores.tsx
@@ -4,11 +4,53 @@ import { NextPage } from "next";
 const Chores: NextPage = () =>{
 
     return(
-        <main className="w-100">
+        <main className="w-100 chores-page">
             <div className="chores">
-                <div className="column"></div>
-                <div className="column"></div>
-                <div className="column"></div>
+                {/* Day */}
+                <div className="column-wrapper">
+                    <div className="column-header">
+                       <h2>Daily</h2>
+                    </div>  
+                    <div className="column">
+                        <ul>
+                            <li>Foo</li>
+                            <li>Foo</li>
+                            <li>Foo</li>
+                            <li>Foo</li>
+                            <li>Foo</li>
+                        </ul>
+                    </div>
+                </div>
+                {/* Week */}
+                <div className="column-wrapper">
+                    <div className="column-header">
+                        <h2>Weekly</h2>
+                    </div>
+                    <div className="column">
+                        <ul>
+                            <li>Foo</li>
+                            <li>Foo</li>
+                            <li>Foo</li>
+                            <li>Foo</li>
+                            <li>Foo</li>
+                        </ul>
+                    </div>
+                </div>
+                {/* Month */}
+                <div className="column-wrapper">
+                    <div className="column-header">
+                        <h2>Monthly</h2>
+                    </div>
+                    <div className="column">
+                        <ul>
+                            <li>Foo</li>
+                            <li>Foo</li>
+                            <li>Foo</li>
+                            <li>Foo</li>
+                            <li>Foo</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </main>
     );
